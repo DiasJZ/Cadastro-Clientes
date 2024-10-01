0ee37ac8990b3bf691cc97d9d98fb3129381d4b5
João Vitor Dias <joaovdias55@gmail.com>
2024-10-01 20:41:24 -0300

    Update Patr.py

--- Patr.py
+++ Patr.py
@@ -8,16 +8,9 @@ from PIL import Image, ImageTk
 # cores
 co0 = "#2e2d2b"  # Preta
 co1 = "#feffff"  # branca
-co2 = "#4fa882"  # verde
 co3 = "#38576b"  # valor
 co4 = "#403d3d"   # letra
-co5 = "#e06636"   # - profit
-co6 = "#038cfc"   # azul
-co7 = "#3fbfb9"   # verde
-co8 = "#263238"   # + verde
 co9 = "#e9edf5"   # + verde
-co10 ="#6e8faf"  
-co11 = "#f2f4f2"
 
 # -------------------------- Criando a janela ---------------------------
 
